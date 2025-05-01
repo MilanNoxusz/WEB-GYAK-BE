bfb3102865a31af5711e759c392106f3ed3607c3
Muskó Milán <161326850+MilanNoxusz@users.noreply.github.com>
2025-05-01 13:23:35 +0200

    commit

---
+++ templates/pages/regisztral.tpl.php
@@ -0,0 +1,16 @@
+<h2>Regisztráció</h2>
+<form method="post" action="regisztral.php">
+    <label for="csn">Családi név:</label>
+    <input type="text" id="csn" name="csn" required>
+    <br>
+    <label for="un">Utónév:</label>
+    <input type="text" id="un" name="un" required>
+    <br>
+    <label for="login">Felhasználónév:</label>
+    <input type="text" id="login" name="login" required>
+    <br>
+    <label for="password">Jelszó:</label>
+    <input type="password" id="password" name="password" required>
+    <br>
+    <button type="submit">Regisztráció</button>
+</form>
